948d9f6212857cd97e471f2e59ae610874af3d28
Francisco <francisco.mc.ids@gmail.com>
2024-01-25 19:28:30 -0400

    I forget Refactor, Firebase, Redesign...

--- lib/screens/add_recipe_screen.dart
+++ lib/screens/add_recipe_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:the_recipes/controllers/add_recipe_controller.dart';
 
 class AddRecipeScreen extends StatelessWidget {
   const AddRecipeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    AddRecipeController recipeController = Get.put(AddRecipeController());
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -36,90 +39,115 @@ class AddRecipeScreen extends StatelessWidget {
                       child: ClipRRect(
                         borderRadius: BorderRadius.circular(10),
                         child: Container(
-                          width: MediaQuery.of(context).size.width,
-                          height: MediaQuery.of(context).size.width,
-                          decoration: const BoxDecoration(
-                            color: Colors.black12,
-                            boxShadow: [
+                          width: MediaQuery.of(context).size.width * 0.6,
+                          height: MediaQuery.of(context).size.width * 0.6,
+                          decoration: BoxDecoration(
+                            boxShadow: const [
                               BoxShadow(
-                                color: Colors.black12,
+                                color: Colors.transparent,
                                 blurRadius: 80,
                               ),
                             ],
+                            border: Border.all(
+                              color: Colors.deepOrange,
+                              width: 3,
+                              strokeAlign: BorderSide.strokeAlignInside,
+                            ),
+                            borderRadius: BorderRadius.circular(10),
                           ),
                           child: const Icon(
                             Icons.add_a_photo,
-                            size: 80,
-                            color: Colors.black38,
+                            size: 48,
+                            color: Colors.deepOrange,
                           ),
                         ),
                       ),
                     ),
                     const SizedBox(height: 16),
-                    TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Nombre de la receta',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
+                    Obx(() {
+                      return TextFormField(
+                        onChanged: recipeController.recipeTitle,
+                        decoration: InputDecoration(
+                          labelText: 'Título de la receta',
+                          errorText: recipeController.recipeTitleValidate.value,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
                         ),
-                      ),
-                    ),
+                      );
+                    }),
                     const SizedBox(height: 16),
-                    TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Descripción',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
+                    Obx(() {
+                      return TextFormField(
+                        onChanged: recipeController.recipeDescription,
+                        decoration: InputDecoration(
+                          labelText: 'Descripción de la receta',
+                          errorText:
+                              recipeController.recipeDescriptionValidate.value,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
                         ),
-                      ),
-                    ),
+                      );
+                    }),
                     const SizedBox(height: 16),
-                    TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Ingredientes',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
+                    Obx(() {
+                      return TextFormField(
+                        onChanged: recipeController.recipeIngredients,
+                        keyboardType: TextInputType.multiline,
+                        maxLines: null,
+                        minLines: 3,
+                        decoration: InputDecoration(
+                          labelText: 'Tiempo de preparación',
+                          errorText:
+                              recipeController.recipeIngredientsValidate.value,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          helperText: 'Separar en cada línea cada ingrediente',
                         ),
-                      ),
-                    ),
+                      );
+                    }),
                     const SizedBox(height: 16),
-                    TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Pasos',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
+                    Obx(() {
+                      return TextFormField(
+                        onChanged: recipeController.recipeDirections,
+                        keyboardType: TextInputType.multiline,
+                        maxLines: null,
+                        minLines: 3,
+                        decoration: InputDecoration(
+                          labelText: 'Pasos de la receta',
+                          errorText:
+                              recipeController.recipeDirectionsValidate.value,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          helperText: 'Separar en cada línea cada paso',
                         ),
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-                    TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Tiempo de preparación',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                    ),
+                      );
+                    }),
                     const SizedBox(height: 16),
                     Row(
                       children: [
                         Expanded(
-                          child: FilledButton(
-                            onPressed: () {},
-                            style: ElevatedButton.styleFrom(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10),
+                          child: Obx(() {
+                            return FilledButton(
+                              onPressed: recipeController.addRecipe.value,
+                              style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
                               ),
-                            ),
-                            child: const Text(
-                              'Agregar Receta',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 16,
-                                fontWeight: FontWeight.w600,
+                              child: const Text(
+                                'Agregar Receta',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w600,
+                                ),
                               ),
-                            ),
-                          ),
+                            );
+                          }),
                         ),
                       ],
                     ),
