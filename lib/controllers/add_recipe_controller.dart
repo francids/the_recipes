a4749cd77f5ace091eb5200eae6432023171a4a5
Francisco <drmalito.weid@gmail.com>
2024-02-06 16:27:30 -0400

    Funcionalidad: Agregar Receta de Firestore y Storage Implementación de ImagePicker y Firebase Storage

--- lib/controllers/add_recipe_controller.dart
+++ lib/controllers/add_recipe_controller.dart
@@ -1,113 +1,56 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:the_recipes/controllers/recipe_controller.dart';
 
 class AddRecipeController extends GetxController {
-  RxString title = ''.obs;
-  RxString image = ''.obs;
-  RxString description = ''.obs;
-  RxString ingredients = ''.obs;
-  RxString directions = ''.obs;
+  final storage = FirebaseStorage.instance;
+  RecipeController recipeController = RecipeController();
 
-  RxnString recipeTitleValidate = RxnString(null);
-  RxnString recipeImageValidate = RxnString(null);
-  RxnString recipeDescriptionValidate = RxnString(null);
-  RxnString recipeIngredientsValidate = RxnString(null);
-  RxnString recipeDirectionsValidate = RxnString(null);
+  Future<void> refreshRecipes() async {
+    await recipeController.refreshRecipes();
+  }
 
-  Rxn<Function()> addRecipe = Rxn<Function()>(null);
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+  TextEditingController ingredientsController = TextEditingController();
+  TextEditingController directionsController = TextEditingController();
+  String? fileName;
+  var fullPath = "".obs;
+  File? image;
 
-  @override
-  void onInit() {
-    super.onInit();
-    debounce<String>(
-      title,
-      titleValidate,
-      time: const Duration(milliseconds: 500),
-    );
-    debounce<String>(
+  void setImagePath(XFile file) {
+    fileName = DateTime.now().toString().removeAllWhitespace + file.name;
+    fullPath.value = file.path;
+    image = File(file.path);
+    update();
+  }
+
+  Future<void> uploadImage() async {
+    EasyLoading.show(status: 'Subiendo imagen...');
+    await storage.ref('recipe-images/$fileName').putFile(image!);
+    EasyLoading.showSuccess('Imagen subida');
+  }
+
+  Future<String> getImageUrl() async {
+    return await storage.ref('recipe-images/$fileName').getDownloadURL();
+  }
+
+  Future<void> addRecipe() async {
+    EasyLoading.show(status: 'Agregando receta...');
+    await uploadImage();
+    String image = await getImageUrl();
+    await recipeController.addRecipe(
+      titleController.text,
+      descriptionController.text,
       image,
-      imageValidate,
-      time: const Duration(milliseconds: 500),
+      ingredientsController.text.split('\n'),
+      directionsController.text.split('\n'),
     );
-    debounce<String>(
-      description,
-      descriptionValidate,
-      time: const Duration(milliseconds: 500),
-    );
-    debounce<String>(
-      ingredients,
-      ingredientsValidate,
-      time: const Duration(milliseconds: 500),
-    );
-    debounce<String>(
-      directions,
-      directionsValidate,
-      time: const Duration(milliseconds: 500),
-    );
-  }
-
-  void titleValidate(String value) {
-    addRecipe.value = null;
-    if (value.isEmpty) {
-      recipeTitleValidate.value = 'El título es requerido';
-    } else {
-      recipeTitleValidate.value = null;
-      addRecipe.value = addRecipeValidate();
-    }
-  }
-
-  void imageValidate(String value) {
-    addRecipe.value = null;
-    if (value.isEmpty) {
-      recipeImageValidate.value = 'La imagen es requerida';
-    } else {
-      recipeImageValidate.value = null;
-      addRecipe.value = addRecipeValidate();
-    }
-  }
-
-  void descriptionValidate(String value) {
-    addRecipe.value = null;
-    if (value.isEmpty) {
-      recipeDescriptionValidate.value = 'La descripción es requerida';
-    } else {
-      recipeDescriptionValidate.value = null;
-      addRecipe.value = addRecipeValidate();
-    }
-  }
-
-  void ingredientsValidate(String value) {
-    addRecipe.value = null;
-    if (value.isEmpty) {
-      recipeIngredientsValidate.value = 'Los ingredientes son requeridos';
-    } else {
-      recipeIngredientsValidate.value = null;
-      addRecipe.value = addRecipeValidate();
-    }
-  }
-
-  void directionsValidate(String value) {
-    addRecipe.value = null;
-    if (value.isEmpty) {
-      recipeDirectionsValidate.value = 'Los pasos son requeridos';
-    } else {
-      recipeDirectionsValidate.value = null;
-      addRecipe.value = addRecipeValidate();
-    }
-  }
-
-  void recipeTitle(String value) => title.value = value;
-
-  void recipeImage(String value) => image.value = value;
-
-  void recipeDescription(String value) => description.value = value;
-
-  void recipeIngredients(String value) => ingredients.value = value;
-
-  void recipeDirections(String value) => directions.value = value;
-
-  Future<bool> Function() addRecipeValidate() {
-    return () async {
-      return true;
-    };
+    EasyLoading.showSuccess('Receta agregada');
   }
 }
