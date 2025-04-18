d6b89449349703fd8ff1be4713f6a1d5a124133b
francids <francisco.mc.ids@gmail.com>
2025-04-18 18:22:28 -0400

    Actualizar el uso de comillas en env.dart y mostrar la versión de la app en settings_screen.dart

--- lib/env/env.dart
+++ lib/env/env.dart
@@ -1,9 +1,12 @@
-import 'package:envied/envied.dart';
+import "package:envied/envied.dart";
 
-part 'env.g.dart';
+part "env.g.dart";
 
 @Envied()
 abstract class Env {
-  @EnviedField(varName: 'TRA_SECRET_KEY', obfuscate: true)
+  @EnviedField(varName: "TRA_SECRET_KEY", obfuscate: true)
   static String TRA_SECRET_KEY = _Env.TRA_SECRET_KEY;
+
+  @EnviedField(varName: "APP_VERSION", optional: true)
+  static String APP_VERSION = _Env.APP_VERSION;
 }
