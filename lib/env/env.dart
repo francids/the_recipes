195dc2e2ee08ac0bee22191bbc88e0664367a302
Francisco Mesa <francisco.mc.ids@gmail.com>
2025-08-30 17:54:32 -0400

    Eliminar la clave secreta TRA_SECRET_KEY del entorno

--- lib/env/env.dart
+++ lib/env/env.dart
@@ -4,9 +4,6 @@ part "env.g.dart";
 
 @Envied()
 abstract class Env {
-  @EnviedField(varName: "TRA_SECRET_KEY", obfuscate: true)
-  static String TRA_SECRET_KEY = _Env.TRA_SECRET_KEY;
-
   @EnviedField(varName: "APP_VERSION", optional: true)
   static String APP_VERSION = _Env.APP_VERSION;
 }
